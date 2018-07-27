0a5b462d1a2363fa532fde96db001551538ab98d
FRANCO JESUS MAMANI POZO <mamanipozofrancojesus@gmail.com>
2018-07-27 16:14:15 -0400

    Automania cliente version 0.1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AuthService} from './auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    constructor(
+        protected authService: AuthService,
+        private router: Router) {  }
+
+    logout() {
+        this.authService.logout();
+        this.router.navigate(['']);
+/*        this.authService.logout()
+            .subscribe((res: any) => {
+                if (res.deslogueo) {
+                    localStorage.removeItem('token');
+                    this.router.navigate(['']);
+                }
+            }, (error: any) => {
+                console.log(error.error.mensaje);
+            });*/
+    }
 }
